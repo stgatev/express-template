4a82391eef62a6a73343e5a7c95d04ac7f302192
Stan Gatev <stang@prime8consulting.com>
2017-11-09 11:28:50 -0800

    Thu Nov  9 11:28:50 PST 2017

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,9 +1,9 @@
 'use strict';
 
- import { UsersRepository as Users } from '../lib/UsersRepository';
+import { UsersRepository as Users } from '../lib/UsersRepository';
 
- module.exports = {
-     get: function users_get(req, res) {
-         res.json(Users.all())
-     }
- };
+module.exports = {
+    get: function users_get(req, res) {
+        res.json(Users.all())
+    }
+};
