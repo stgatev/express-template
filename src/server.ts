3762407e2aa400ae41e7393b38fa08147a107c5b
Stan Gatev <stang@prime8consulting.com>
2018-01-05 12:28:59 -0800

    Fri, Jan  5, 2018 12:28:59 PM

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,6 @@ import * as http from 'http';
 import app from './app';
 
 const config = app.get('config');
-const port = config.get('server.port') || 8000;
+const port = process.env.PORT || (config.has('server.port') ? config.get('server.port') : undefined) || 8000;
 const server = http.createServer(app);
 server.listen(port);
